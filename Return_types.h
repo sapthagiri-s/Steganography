2c52db8340f34466c5ebd9d624998a77ce841fea
Sapthagiri Sanmugam <sapthagiri.s@emertxe.com>
2025-03-23 14:01:52 +0530

    Return_types.h

--- Return_types.h
+++ Return_types.h
@@ -1,6 +1,6 @@
 #include<stdio.h> // Include standard input-output header file
-#ifndef TYPES_H // Check if TYPES_H is not defined
-#define TYPES_H // Define TYPES_H
+#ifndef RETURN_TYPES_H // Check if TYPES_H is not defined
+#define RETURN_TYPES_H // Define TYPES_H
 
 /* User defined types */
 typedef unsigned int uint; // Define uint as an alias for unsigned int
